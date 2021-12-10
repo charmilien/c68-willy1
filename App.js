457ce56a8e275070ff5c69043724c4434c905f7c
charmilien <72658425+charmilien@users.noreply.github.com>
2021-12-10 16:11:40 +0530

    Add files via upload
    Willy App 1st file c68

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {View} from 'react-native'
+import BottomTabNavigator from './component/bottomNavigator'
+
+export default class App extends Component {
+  render() {
+    return (
+    
+   
+<BottomTabNavigator />
+  
+    
+    
+    )
+  }
+}
